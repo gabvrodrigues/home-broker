f697661cc8fe3552c6a6b5ef2a70ac9f01b4bd35
Gabriel <gabriel@gebit.io>
2020-09-14 01:30:30 -0300

    adding quote list methods

--- client.py
+++ client.py
@@ -1,6 +1,27 @@
 import Pyro4
 
-name = input("What is your name? ").strip()
+homeBroker = Pyro4.Proxy("PYRONAME:home.broker.server")   # use name server object lookup uri shortcut
+def printMenu():
+    print("-----------HOME BROKER-----------")
+    print("1) Adicionar ação às minhas cotações")
+    print("2) Remover ação às minhas cotações")
+    print("3) Ver minha lista de cotações")
 
-greeting_maker = Pyro4.Proxy("PYRONAME:home.broker.server")    # use name server object lookup uri shortcut
-print(greeting_maker.get_fortune(name))
+while True:
+    printMenu()
+    option = input("Escolha: ").strip()
+
+    if(option == "1"):
+        code = input("Código: ").strip()
+        print(homeBroker.addStockToQuoteList(code))
+    elif(option == "2"):
+        code = input("Código: ").strip()
+        print(homeBroker.removeStockToQuoteList(code))
+    elif(option == "3"):
+        for stock in homeBroker.getQuoteList():
+            print("{0} - {1}".format(stock["code"], stock["price"]))
+
+
+
+
+
--- server.py
+++ server.py
@@ -1,15 +1,38 @@
 import Pyro4
 
 @Pyro4.expose
-class GreetingMaker(object):
-    def get_fortune(self, name):
-        return "Hello, {0}. Here is your fortune message:\n" \
-               "Tomorrow's lucky number is 12345678.".format(name)
+class HomeBroker(object):
+    stocks = [{"code": "PETR4", "price": 15.90}, {"code": "VALE3", "price" : 30.20}]
+
+    def __init__(self):
+        self.quoteList = []
+
+    def addStockToQuoteList(self, code):
+        stockToSave = self.findStock(code)
+        if(not stockToSave):
+            return "Ação não encontrada!"
+        self.wishList.append(self.findStock(code))
+        return "A ação {0} foi adicionada a sua lista de cotações".format(code)
+    
+    def removeStockToQuoteList(self, code):
+        stockToRemove = self.findStock(code)
+        if(not stockToRemove):
+            return "Ação não encontrada!"
+        self.wishList.remove(self.findStock(code))
+        return "A ação {0} foi removida a sua lista de cotações".format(code)
+
+    def getQuoteList(self):
+        return self.quoteList
+
+    def findStock(self, code):
+        for stock in self.stocks:
+            if(stock['code'] == code):
+                return stock
 
 daemon = Pyro4.Daemon()                # make a Pyro daemon
 ns = Pyro4.locateNS()                  # find the name server
-uri = daemon.register(GreetingMaker)   # register the greeting maker as a Pyro object
-ns.register("home.broker.server", uri)   # register the object with a name in the name server
+uri = daemon.register(HomeBroker)      # register the greeting maker as a Pyro object
+ns.register("home.broker.server", uri) # register the object with a name in the name server
 
 print("Ready.")
 daemon.requestLoop()                   # start the event loop of the server to wait for calls
