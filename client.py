8c6b5b3f521aa1a5a5bfcb846255452b3b71c8bf
Gabriel <gabriel@gebit.io>
2020-09-14 23:46:56 -0300

    adicionando métodos de ordem de compra e venda

--- client.py
+++ client.py
@@ -1,11 +1,36 @@
 import Pyro4
 
-homeBroker = Pyro4.Proxy("PYRONAME:home.broker.server")   # use name server object lookup uri shortcut
+# homeBroker = Pyro4.Proxy("PYRONAME:home.broker.server")   # use name server object lookup uri shortcut
+# Pyro4.config.COMMTIMEOUT=0.5
+
+class Callback(object):
+    def crash(self):
+        a=1
+        b=0
+        return a//b
+    @Pyro4.expose
+    @Pyro4.callback
+    def call(self):
+        print("callback received")
+        return self.crash()
+
+daemon = Pyro4.core.Daemon()
+callback = Callback()
+daemon.register(callback)
+
+homeBroker = Pyro4.Proxy("PYRONAME:home.broker.server")
+
+# print("waiting for callbacks to arrive...")
+# print("(ctrl-c/break the program once it's done)\n")
+# daemon.requestLoop()
+
 def printMenu():
     print("-----------HOME BROKER-----------")
     print("1) Adicionar ação às minhas cotações")
     print("2) Remover ação às minhas cotações")
     print("3) Ver minha lista de cotações")
+    print("4) Comprar ação")
+    print("5) Vender ação")
 
 while True:
     printMenu()
@@ -20,6 +45,18 @@ while True:
     elif(option == "3"):
         for stock in homeBroker.getQuoteList():
             print("{0} - {1}".format(stock["code"], stock["price"]))
+    elif(option == "4"):
+        code = input("Código: ").strip()
+        quantity = input("Quantidade: ").strip()
+        price = input("Preço: ").strip()
+        print(homeBroker.buyStock(code, quantity, price))
+        homeBroker._pyroOneway.add("doCallback")
+        homeBroker.doCallback(callback)
+    elif(option == "5"):
+        code = input("Código: ").strip()
+        quantity = input("Quantidade: ").strip()
+        price = input("Preço: ").strip()
+        print(homeBroker.sellStock(code, quantity, price))
 
 
 
--- server.py
+++ server.py
@@ -1,11 +1,52 @@
 import Pyro4
+import uuid
+
 
 @Pyro4.expose
 class HomeBroker(object):
     stocks = [{"code": "PETR4", "price": 15.90}, {"code": "VALE3", "price" : 30.20}]
+    bookSell = []
+    bookBuy = []
 
     def __init__(self):
         self.quoteList = []
+        self.myStocks = []
+    
+    def buyStock(self, code, quantity, price):
+        stock = self.findStock(code)
+        if(not stock):
+            return "Ação não encontrada!"
+        order = {"id": uuid.uuid4(), "code": code, "quantity": quantity, "price": price}
+        self.bookBuy.append(order)
+        self.executeOrderBuy(order)
+        return "Ordem de compra criada com sucesso!"
+
+    def executeOrderBuy(self, orderToExecute):
+        for order in self.bookSell:
+            if(order["code"] == orderToExecute["code"] and order["quantity"] >= orderToExecute["quantity"] 
+            and order['price'] == orderToExecute["price"]):
+                self.myStocks.append({"code": orderToExecute["code"], "quantity": orderToExecute["quantity"], "price": orderToExecute["price"]})
+                self.bookSell.remove(order)
+                self.bookBuy.remove(self.findOrderBuy(orderToExecute['id']))
+                #Notificar compra e venda
+    
+    def sellStock(self, code, quantity, price):
+        stock = self.findStock(code)
+        if(not stock):
+            return "Ação não encontrada!"
+        order = {"id": uuid.uuid4(), "code": code, "quantity": quantity, "price": price}
+        self.bookSell.append(order)
+        self.executeOrderSell(order)
+        return "Ordem de venda criada com sucesso!"
+
+    def executeOrderSell(self, orderToExecute):
+        for order in self.bookBuy:
+            if(order["code"] == orderToExecute["code"] and order["quantity"] >= orderToExecute["quantity"] 
+            and order['price'] == orderToExecute["price"]):
+                self.myStocks.append({"code": orderToExecute["code"], "quantity": orderToExecute["quantity"], "price": orderToExecute["price"]})
+                self.bookBuy.remove(order)
+                self.bookSell.remove(self.findOrderSell(orderToExecute['id']))
+                #Notificar compra e venda
 
     def addStockToQuoteList(self, code):
         stockToSave = self.findStock(code)
@@ -29,10 +70,29 @@ class HomeBroker(object):
             if(stock['code'] == code):
                 return stock
 
+    def findOrderBuy(self, id):
+        for order in self.bookBuy:
+            if(order['id'] == id):
+                return order
+
+    def findOrderSell(self, id):
+        for order in self.bookSell:
+            if(order['id'] == id):
+                return order
+    
+    def doCallback(self, callback):
+        try:
+            callback.call()
+        except:
+            print("got an exception from the callback.")
+            print("".join(Pyro4.util.getPyroTraceback()))
+        print("server: callbacks done")
+
 daemon = Pyro4.Daemon()                # make a Pyro daemon
 ns = Pyro4.locateNS()                  # find the name server
 uri = daemon.register(HomeBroker)      # register the greeting maker as a Pyro object
 ns.register("home.broker.server", uri) # register the object with a name in the name server
+
 
 print("Ready.")
 daemon.requestLoop()                   # start the event loop of the server to wait for calls
